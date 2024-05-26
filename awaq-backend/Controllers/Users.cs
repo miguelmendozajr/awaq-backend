6208078aacc3dc89d81460be1b5761ffeaf24e3e
Miguel Mendoza <miguelmendoza@192.168.1.9>
2024-05-25 21:06:19 -0600

    add user controllers

--- awaq-backend/Controllers/Users.cs
+++ awaq-backend/Controllers/Users.cs
@@ -11,6 +11,7 @@ namespace awaq_backend.Controllers
 {
     public class User
     {
+        public int? ID { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
@@ -41,13 +42,14 @@ namespace awaq_backend.Controllers
 
                     MySqlCommand cmd = new MySqlCommand();
                     cmd.Connection = connection;
-                    cmd.CommandText = "Select user_id, email, name, password, r.role AS role, joinedAt from users u JOIN roles r ON u.role_id = r.role_id;";
+                    cmd.CommandText = "Select user_id, email, name, password, r.role AS role, joinedAt from users u JOIN roles r ON u.role_id = r.role_id ORDER BY joinedAt DESC;";
 
                     using (var reader = await cmd.ExecuteReaderAsync())
                     {
                         while (reader.Read())
                         {
                             User usr = new User();
+                            usr.ID = (int?)reader["user_id"];
                             usr.Name = reader["name"].ToString();
                             DateTime joinedAt = Convert.ToDateTime(reader["joinedAt"]);
                             usr.Password = reader["password"].ToString();
@@ -84,11 +86,33 @@ namespace awaq_backend.Controllers
         {
             try
             {
+
+
                 List<User> users = new List<User>();
                 string? connectionString = _configuration.GetConnectionString("myDb1");
+
+
                 using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
                     await connection.OpenAsync();
+
+                    MySqlCommand cmd2 = new MySqlCommand();
+                    cmd2.Connection = connection;
+                    cmd2.CommandText = "SELECT email FROM users where email=@Email";
+                    cmd2.Parameters.AddWithValue("@Email", request.Email);
+                    using (var reader = await cmd2.ExecuteReaderAsync())
+                    {
+                        while (reader.Read())
+                        {
+                            if (reader["email"].ToString() == request.Email)
+                            {
+                                return BadRequest("El correo electrónico ya está vinculado a una cuenta.");
+
+                            }
+   
+                        }
+                    }
+
 
                     MySqlCommand cmd = new MySqlCommand();
                     cmd.Connection = connection;
@@ -108,9 +132,155 @@ namespace awaq_backend.Controllers
                     {
                         return Ok(new { Message = "Usuario creado exitosamente." });
                     }
+
                 }
 
                 return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        public class EditUserRoleRequest
+        {
+            public required int Role { get; set; }
+        }
+
+        [HttpPut]
+        [Route("api/user/{id}/role")]
+        public async Task<IActionResult> UpdateUserRole(int id, [FromBody] EditUserRoleRequest request)
+        {
+            try
+            {
+                string? connectionString = _configuration.GetConnectionString("myDb1");
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    MySqlCommand cmd2 = new MySqlCommand();
+                    cmd2.Connection = connection;
+                    cmd2.CommandText = "UPDATE users SET role_id = @Role WHERE user_id = @Id";
+                    cmd2.Parameters.AddWithValue("@Id", id);
+                    cmd2.Parameters.AddWithValue("@Role", request.Role);
+                    await cmd2.ExecuteNonQueryAsync();
+                }
+
+                return Ok("Usuario editado exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/user/{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            try
+            {
+                string? connectionString = _configuration.GetConnectionString("myDb1");
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    MySqlCommand cmd2 = new MySqlCommand();
+                    cmd2.Connection = connection;
+                    cmd2.CommandText = "SELECT name, birthDate, phoneNumber, profile_picture FROM users WHERE user_id = @Id";
+                    cmd2.Parameters.AddWithValue("@Id", id);
+
+                    // Execute the command and retrieve data
+                    using (MySqlDataReader reader = (MySqlDataReader)await cmd2.ExecuteReaderAsync())
+                    {
+                        if (reader.Read())
+                        {
+                            // Retrieve data from the reader
+                            string? name = reader.IsDBNull(0) ? null : reader.GetString(0);
+                            DateTime? birthDate = reader.IsDBNull(1) ? null : (DateTime?)reader.GetDateTime(1);
+                            string? phoneNumber = reader.IsDBNull(2) ? null : reader.GetString(2);
+                            string? profilePicture = reader.IsDBNull(3) ? null : reader.GetString(3);
+
+                            // Construct response object
+                            var responseData = new
+                            {
+                                Name = name,
+                                BirthDate = birthDate,
+                                PhoneNumber = phoneNumber,
+                                ProfilePicture = profilePicture
+                            };
+
+                            return Ok(responseData);
+                        }
+                        else
+                        {
+                            // User not found
+                            return NotFound();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        public class EditUserRequest
+        {
+            public string? Name { get; set; }
+            public string? Phone { get; set; }
+            public DateTime? Date { get; set; }
+        }
+
+        [HttpPut]
+        [Route("api/user/{id}")]
+        public async Task<IActionResult> UpdateUserData(int id, [FromBody] EditUserRequest request)
+        {
+            try
+            {
+                string? connectionString = _configuration.GetConnectionString("myDb1");
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+                    MySqlCommand cmd2 = new MySqlCommand();
+                    cmd2.Connection = connection;
+
+                    // Start building the query
+                    List<string> setClauses = new List<string>();
+                    if (request.Name != null)
+                    {
+                        setClauses.Add("name=@Name");
+                        cmd2.Parameters.AddWithValue("@Name", request.Name);
+                    }
+                    if (request.Date != null)
+                    {
+                        setClauses.Add("birthDate=@BirthDate");
+                        cmd2.Parameters.AddWithValue("@BirthDate", request.Date);
+                    }
+                    if (request.Phone != null)
+                    {
+                        setClauses.Add("phoneNumber=@PhoneNumber");
+                        cmd2.Parameters.AddWithValue("@PhoneNumber", request.Phone);
+                    }
+
+                    string setClause = string.Join(", ", setClauses);
+
+                    if (string.IsNullOrEmpty(setClause))
+                    {
+                        return BadRequest("No valid fields provided to update.");
+                    }
+
+                    cmd2.CommandText = $"UPDATE users SET {setClause} WHERE user_id=@Id";
+                    cmd2.Parameters.AddWithValue("@Id", id);
+
+                    await cmd2.ExecuteNonQueryAsync();
+                }
+
+                return Ok("Usuario editado exitosamente.");
             }
             catch (Exception ex)
             {
